e2c113788f7cec8146f032068832f14735bd4e4f
John Wass <jwass3@gmail.com>
2022-08-01 08:56:09 -0400

    opts

--- src/echo_pgns.rs
+++ src/echo_pgns.rs
@@ -1,12 +1,26 @@
-use canparse::pgn::{ParseMessage, PgnLibrary, SpnDefinition};
+use canparse::pgn::PgnLibrary;
+use clap::Parser;
 use example_j1939_rs::pgn;
+use git_version::git_version;
+
+const GIT_VERSION: &str = git_version!();
+
+#[derive(Parser)]
+#[clap(name = "CanEchoPGN", version = GIT_VERSION)]
+pub struct Opts {
+    #[clap(long, default_value = "data/j1939_utf8.dbc")]
+    pub dbc: String,
+
+    #[clap(default_value = "vcan0")]
+    pub socket: String,
+}
 
 // canplayer -v -g 100 -I data/lfe0.log vcan0=can0
 fn main() {
-    let lib = PgnLibrary::from_dbc_file("data/j1939_utf8.dbc").unwrap();
+    let opts: Opts = Opts::parse();
 
-    // todo;; externalize socket address
-    let rx = socketcan::CANSocket::open("vcan0").expect("could not open vcan");
+    let lib = PgnLibrary::from_dbc_file(opts.dbc).unwrap();
+    let rx = socketcan::CANSocket::open(&opts.socket).expect("could not open vcan");
 
     println!("listening...");
     loop {
